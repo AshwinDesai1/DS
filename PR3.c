875cc756d40d1aa539da28b9e0edb29a691ca22b
Ashwin Rabari <166016321+AshwinDesai1@users.noreply.github.com>
2024-09-01 12:15:38 +0530

    Update PR3.c

--- PR3.c
+++ PR3.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include<stdlib.h>
 #define max 5
@@ -148,6 +149,73 @@ void peep(int Stack[],int *top1,int *top2)
         goto x;
     }
 }
+void modify(int Stack[],int *top1,int *top2)
+{
+    int stack_choice=0,ele,i,f=0;
+    x:
+    printf("\n1 For Modify in Stack 1 ");
+    printf("\n2 For Modify in Stack 2 ");
+    printf("\nEnter your choice : ");
+    scanf("%d",&stack_choice);
+    
+    if(stack_choice==1)
+    {
+        if(*top1==-1)
+        {
+            printf("\nStack 1 is underflow.");
+        }
+        else
+        {
+            printf("\nEnter element that you want to Modify : ");
+            scanf("%d",&ele);
+            for(i=0;i<=(*top1);i++)
+            {
+                if(Stack[i]==ele)
+                {
+                    printf("\nEnter new element : ");
+                    scanf("%d",&Stack[i]);
+                    printf("\n%d replaced for %d in Stack 1.",ele,Stack[i]);
+                    f++;
+                }
+            }
+            if(f==0)
+            {
+                printf("\nElement not found in Stack 1.");
+            }
+        }
+    }
+    else if(stack_choice==2)
+    {
+        if(*top2==max)
+        {
+            printf("\nStack 2 is underflow.");
+        }
+        else
+        {
+            printf("\nEnter element that you want to Modify : ");
+            scanf("%d",&ele);
+            for(i=max-1;i>=(*top2);i--)
+            {
+                if(Stack[i]==ele)
+                {
+                    printf("\nEnter new element : ");
+                    scanf("%d",&Stack[i]);
+                    printf("\n%d replaced for %d in Stack 2.",ele,Stack[i]);
+                    f++;
+                }
+            }
+            if(f==0)
+            {
+                printf("\nElement not found in Stack 2.");
+            }
+        }
+    }
+    else
+    {
+        printf("\nPlease enter correct stack number!.");
+        goto x;
+    }
+}
 
 int main() {
    int Stack[max],t1=-1,t2=max,*top1=&t1,*top2=&t2,choice;
@@ -156,7 +224,8 @@ int main() {
        printf("\n\n1 For push ");
        printf("\n2 For pop ");
        printf("\n3 For peep ");
-       printf("\n4 For exit");
+       printf("\n4 For modify");
+       printf("\n5 For exit");
        printf("\nEnter your choice : ");
        scanf("%d",&choice);
        
@@ -172,6 +241,9 @@ int main() {
                 peep(Stack,top1,top2);
                 break;
             case 4:
+                modify(Stack,top1,top2);
+                break;
+            case 5:
                 exit(0);
                 break;
             default:
